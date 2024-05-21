ab4fc911b9271262c9982d815cc6a228fc4ec89a
Platon61633 <peredreevplaton@gmail.com>
2024-05-22 00:25:53 +0300

    gg

--- src/App.js
+++ src/App.js
@@ -11,11 +11,10 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
+          href="./assets/file.pptx" download>
+            Скачать презентацию по руссскому
         </a>
       </header>
     </div>
